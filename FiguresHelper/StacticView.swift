ffe22d0467d5d2c568487e180634b4f7d475ef85
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-01 09:54:56 -0500

    Created Static View

---
+++ FiguresHelper/StacticView.swift
@@ -0,0 +1,20 @@
+//
+//  StacticView.swift
+//  FiguresHelper
+//
+//  Created by Luke Newbigging on 2021-12-01.
+//
+
+import SwiftUI
+
+struct StacticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StacticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StacticView()
+    }
+}
