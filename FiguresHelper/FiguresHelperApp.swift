3880358fdebd6fdf86c96cd77f872b7cbed5fc7c
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-01 09:54:17 -0500

    Initial Commit

---
+++ FiguresHelper/FiguresHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresHelperApp.swift
+//  FiguresHelper
+//
+//  Created by Luke Newbigging on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct FiguresHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
